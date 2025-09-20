0a05365e7896ea7d215be5404ff62d16ff5998bc
NicoCarbonel <nicolas.carbonel@anima.edu.uy>
2025-09-19 22:13:18 -0300

    add form for person

--- mi-proyecto/src/pages/Sign_In_User.jsx
+++ mi-proyecto/src/pages/Sign_In_User.jsx
@@ -1,8 +1,66 @@
+import { useState } from "react";
+
 export default function Sign_In_User() {
+  const [person, setPerson] = useState({
+    email: "",
+    password: "",
+    name: "",
+    ci:"",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setPerson((prev) => ({ ...prev, [id]: value }));
+  };
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Datos de la persona:", person);
+  };
   return (
     <div className="p-10">
       <h1 className="text-4xl font-bold text-yellow-600">Sign In User se cargó 👌</h1>
-      <p className="mt-3 text-gray-700">Aca van a ir el inicio de sesion de las Personas </p>
+      <form >
+        <label htmlFor="email">Email:</label>
+        <input type="email"
+          id="email"
+          value={person.email}
+          onChange={handleChange}
+          placeholder="Cual es su email?"
+          required
+        />
+        <br />
+        <label htmlFor="password">Contraseña:</label>
+        <input type="password"
+          id="password"
+          value={person.password}
+          onChange={handleChange}
+          placeholder="Escriba su contraseña"
+          required
+        />
+        <br />
+        <label htmlFor="name">Nombre:</label>
+        <input type="text"
+          id="name"
+          value={person.name}
+          onChange={handleChange}
+          placeholder="Cual es su nombre?"
+          required
+        />
+        <br />
+        <label htmlFor="ci">C.I:</label>
+        <input type="number"
+          id="ci"
+          value={person.ci}
+          onChange={handleChange}
+          placeholder="Cual es su C.I?"
+          required
+        />
+        <br />
+        <button type="submit">
+          Registrar empresa
+        </button>
+      </form>
     </div>
   );
 }
