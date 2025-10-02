adad279a0f518b74616624931e15d64390f0fab1
NicoCarbonel <nicolas.carbonel@anima.edu.uy>
2025-10-02 09:13:36 -0300

    add color palette

--- mi-proyecto/src/pages/Home.jsx
+++ mi-proyecto/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="p-10">
      <Link to={"/sign_in_user"}><button>Registrarce como persona</button></Link>
      <Link to={"/sign_in_company"}><button>Registrarce como empresa</button></Link>
-     <Link to={"/log_in"}><button>Iniciar sesion</button></Link>
+     <button><Link to={"/log_in"}>Iniciar Secion</Link></button>
       <h1 className="text-4xl font-bold text-yellow-600">Home cargó 👌</h1>
       <p className="mt-3 text-gray-700">Router + Tailwind OK.</p>
     </div>
--- mi-proyecto/tailwind.config.js
+++ mi-proyecto/tailwind.config.js
@@ -9,6 +9,28 @@ export default {
       fontFamily: {
         atkinson: ['"Atkinson Hyperlegible"', 'sans-serif'],
       },
+      colors: {
+//----------------------Paleta de colores Claros---------------------
+        soft_honey: "#FFF8E7",   // Miel Diluida
+        creamy_yellow: "#FFF0C2", // Amarillo Cremoso
+        dark_cian: "#1F2937",    // Cian Oscuro
+        graphite_gray: "#4B5563",   // Gris Grafito
+        light_gray: "#E5E7EB", // Azul Grisáceo Claro
+        link_blue: "#1769E0",   // Azul (para los links)
+        dark_turquoise: "#0B7285", // Turquesa Oscuro
+        hard_red: "#DC2626", // Rojo Fuerte
+        golden: "#E69C00",   // Dorado
+        terracotta_blue: "#374151", // Azul Terracota
+        alternative_magenta: "#C21D6B", // Magenta
+//---------------------Paleta de colores Oscuros---------------------
+        disturbing_black: "#111827", // Negro Inquietante  
+        cold_blue: "#D1D5DB", // Azul Frio 
+        carbon_blue: "#374151", // Azul Carbón  
+        vibrant_blue: "#3B82F6", // Azul Vibrante 
+        light_red: "#F87171", // Rojo Claro
+        bright_honey: "#F3B61F", // Miel Brillante
+        softened_magenta: "#C21D6B", // Magenta Suavizado
+      },
     },
   },
   plugins: [],
